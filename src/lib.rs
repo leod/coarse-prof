bd58e5ed81aaa2834b7d5efa5edd6647bca18d31
leod <subtle.frustration@proton.me>
2019-12-03 22:03:03 +0100

    Add some more tests

--- src/lib.rs
+++ src/lib.rs
@@ -344,6 +344,74 @@ impl Profiler {
 #[cfg(test)]
 mod tests {
     #[test]
+    fn test_multiple_roots() {
+        super::reset();
+
+        for i in 0..=5 {
+            if i == 5 {
+                profile!("a");
+            }
+            {
+                profile!("b");
+            }
+        }
+
+        super::PROFILER.with(|p| {
+            let p = p.borrow();
+
+            assert_eq!(p.roots.len(), 2);
+
+            for root in p.roots.iter() {
+                assert!(root.borrow().pred.is_none());
+                assert!(root.borrow().succs.is_empty());
+            }
+
+            assert_eq!(p.roots[0].borrow().name, "b");
+            assert_eq!(p.roots[1].borrow().name, "a");
+
+            assert_eq!(p.roots[0].borrow().num_calls, 6);
+            assert_eq!(p.roots[1].borrow().num_calls, 1);
+        });
+    }
+
+    #[test]
+    fn test_succ_reuse() {
+        use std::ptr;
+
+        super::reset();
+
+        for i in 0..=5 {
+            profile!("a");
+            if i > 2 {
+                profile!("b");
+            }
+        }
+
+        assert_eq!(super::PROFILER.with(|p| p.borrow().roots.len()), 1);
+
+        super::PROFILER.with(|p| {
+            let p = p.borrow();
+
+            assert_eq!(p.roots.len(), 1);
+
+            let root = p.roots[0].borrow();
+            assert_eq!(root.name, "a");
+            assert!(root.pred.is_none());
+            assert_eq!(root.succs.len(), 1);
+            assert_eq!(root.num_calls, 6);
+
+            let succ = root.succs[0].borrow();
+            assert_eq!(succ.name, "b");
+            assert!(ptr::eq(
+                succ.pred.as_ref().unwrap().as_ref(),
+                p.roots[0].as_ref()
+            ));
+            assert!(succ.succs.is_empty());
+            assert_eq!(succ.num_calls, 3);
+        });
+    }
+
+    #[test]
     fn test_reset_during_frame() {
         super::reset();
 
@@ -356,11 +424,17 @@ mod tests {
                     super::reset();
                 }
 
+                assert!(super::PROFILER.with(|p| p.borrow().current.is_some()));
+
                 profile!("d");
             }
         }
 
-        assert!(super::PROFILER.with(|p| p.borrow().roots.is_empty()));
-        assert!(super::PROFILER.with(|p| p.borrow().current.is_none()));
+        super::PROFILER.with(|p| {
+            let p = p.borrow();
+
+            assert!(p.roots.is_empty());
+            assert!(p.current.is_none());
+        });
     }
 }
